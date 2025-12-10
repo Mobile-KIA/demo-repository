6055ec1cadd81cae7190f2fbd7cb2c75c488d98c
Irpunk <irfanfathoni1204@gmail.com>
2025-12-10 13:42:14 +0700

    add folder medis

---
+++ resources/views/medis/pasien/detail.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+    <div class="card-custom">
+        <h4>Detail Pasien: {{ $patient->nama }}</h4>
+
+        <p><b>NIK:</b> {{ $patient->nik }}</p>
+        <p><b>Umur:</b> {{ $patient->umur }}</p>
+        <p><b>Alamat:</b> {{ $patient->alamat }}</p>
+        <p><b>No Telp:</b> {{ $patient->no_telp }}</p>
+
+        <hr>
+
+        <h5>Riwayat Kehamilan:</h5>
+
+        @foreach ($patient->pregnancies as $p)
+            <div class="border p-2 mb-2">
+                <b>Usia Kehamilan:</b> {{ $p->usia_kehamilan }} minggu<br>
+                <b>Berat:</b> {{ $p->berat_badan }} kg<br>
+                <b>Tinggi:</b> {{ $p->tinggi_badan }} cm<br>
+                <b>Tekanan Darah:</b> {{ $p->tekanan_darah }}<br>
+                <b>Keluhan:</b> {{ $p->keluhan ?? '-' }}<br>
+            </div>
+        @endforeach
+    </div>
+@endsection
+
+//testing
